d268b18e02355eed014e44bac16211ae621b0e0a
Cyril-Catoen <cyril.catoen@lapiscine.pro>
2025-05-12 16:37:43 +0200

    create user comment

--- src/Controller/admin/AdminUserController.php
+++ src/Controller/admin/AdminUserController.php
@@ -13,15 +13,18 @@ class AdminUserController extends AbstractController {
 
 	#[Route('/admin/create-user', name: 'admin/create-user')]
 	public function displayCreateUser(Request $request, UserPasswordHasherInterface $userPasswordHasher){
-
+        // On utilise les composants de Symfony pour gérer le hash du Password et la création de l'utilisateur
 
 		if ($request->isMethod('POST')) {
 
+            // Si le formulaire est soumis, on récupère dans deux variables distinctes l'email (identifiant futur de l'user) et le hash du mot de passe généré
 			$email = $request->request->get('email');
 			$password = $request->request->get(key: 'password');
 
+            // On créé un User avec la fonction préconstruite par Symfony
 			$user = new User();
 
+            // Le mot de passe est hashé par la fonction intégrée par Symfony
 			$passwordHashed = $userPasswordHasher->hashPassword($user, $password);
 
 
